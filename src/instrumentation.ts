00f8b6d7227202d3e795f64692c99d24752366a3
Olya Vo <olga.volkova@email.cz>
2026-02-09 01:58:57 +0100

    solve the problem with BroadcastChannel and MSW/node

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,7 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.MOCK_MODE === 'true') {
-    const { server } = await import('../mocks/node');
-    server.listen();
-    console.log('MSW Server listening');
+    const { configureMSW } = await import('../mocks/node');
+    configureMSW();
   }
 }
